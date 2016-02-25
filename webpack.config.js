3a7842d8ee5090a107a5beff54c6175e2c3ec8eb
Irene Robb <irenerobb72@gmail.com>
2016-02-25 23:27:42 +1300

    fixing file name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.jsx'
+    './dist/index.html'
   ],
   module: {
     loaders: [{
