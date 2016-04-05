07e053ea82428fe914445ac43325b0fafda4f802
Irene Robb <irenerobb72@gmail.com>
2016-04-05 10:06:14 -0400

    fixing component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,29 @@
+import React, { Component } from 'react'
+import reactMixin from 'react-mixin'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { connect } from 'react-redux'
+import * as productMovement from '../product-movement'
 
+export class Carticon extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h1>WANEWORK</h1>
+      <a href="#">MAGIC</a>
+      <a href="#">MY FEED</a>
+      <a href="#">TRENDING</a>
+      <input type="text" name="name" placeholder="Find amazing products"></input>
+    )
+  }
+}
 
-<h1>WANEWORK</h1>
-<a href="#">MAGIC</a>
-<a href="#">MY FEED</a>
-<a href="#">TRENDING</a>
+function mapStateToProps(state) {
+  return {
+    products: state.get('products')
+  }
+}
+
+export const CarticonContainer = connect(mapStateToProps, productMovement)(Carticon)
+Status API Training Shop Blog About
